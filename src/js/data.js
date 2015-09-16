fadd450c5b6d6c8c485cb65d79bddcde254a9f32
nltesown <nlte@magic.fr>
2015-09-16 12:43:27 +0200

    Move places to a new file data.js

---
+++ src/js/data.js
@@ -0,0 +1,50 @@
+
+var places = [
+  {
+    groupId: 1,
+    groupName: "Mean Streets",
+    items: [
+      { id: 1, lat: 40.72358, lng: -73.99518, name: "Vieille cathédrale Saint-Patrick" },
+      { id: 2, lat: 40.72176, lng: -73.99707, name: "Volpe Bar" },
+      { id: 3, lat: 40.71816, lng: -73.99794, name: "Mulberry St and Hester St" },
+      { id: 4, lat: 40.7181, lng: -74.0128, name: "The Bridge" },
+      { id: 5, lat: 40.7193, lng: -73.99732, name: "Mulberry St" },
+      { id: 6, lat: 40.72075, lng: -73.9971, name: "394 Broome St" },
+      { id: 7, lat: 40.7312, lng: -74.00167, name: "323 6th Ave - Waverly Theater" }
+    ]
+  },
+  {
+    groupId: 2,
+    groupName: "Taxi Driver",
+    items: [
+      { id: 8, lat: 40.76987, lng: -73.99031, name: "W57th Street" },
+      { id: 9, lat: 40.76041, lng: -73.98745, name: "8th Ave &amp; W47th St." },
+      { id: 10, lat: 40.75992, lng: -73.98782, name: "737 8th Ave - Show and Tell Theater" },
+      { id: 11, lat: 40.77135, lng: -73.98206, name: "Broadway &amp; W 63rd St" },
+      { id: 12, lat: 40.75890, lng: -73.98513, name: "Times Square" },
+      { id: 13, lat: 40.75665, lng: -73.98766, name: "Hilton Theater" },
+      { id: 14, lat: 40.76381, lng: -73.98294, name: "Ed Sullivan Theater" },
+      { id: 15, lat: 40.74335, lng: -73.98413, name: "Park Ave S &amp; E 28th St" },
+      { id: 16, lat: 40.73225, lng: -73.98737, name: "" },
+      { id: 17, lat: 40.73202, lng: -73.98668, name: "" },
+      { id: 18, lat: 40.76804, lng: -73.98237, name: "" },
+      { id: 19, lat: 40.76142, lng: -73.97470, name: "" },
+      { id: 20, lat: 40.76741, lng: -73.98239, name: "" },
+      { id: 21, lat: 40.75847, lng: -73.98469, name: "" },
+      { id: 22, lat: 40.75657, lng: -73.99028, name: "" }
+    ]
+  },
+  {
+    groupId: 3,
+    groupName: "Raging Bull",
+    items: [
+      { id: 23, lat: 40.81637, lng: -73.91529, name: "" },
+      { id: 24, lat: 40.73399, lng: -73.98891, name: "" },
+      { id: 25, lat: 40.72958, lng: -74.00547, name: "" },
+      { id: 26, lat: 40.73176, lng: -73.98913, name: "" },
+      { id: 27, lat: 40.76436, lng: -73.97189, name: "" },
+      { id: 28, lat: 40.76834, lng: -73.9882, name: "" },
+      { id: 29, lat: 40.76138, lng: -73.98527, name: "" }
+    ]
+  }
+];
