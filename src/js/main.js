fc8c01e701bb2a47a23135a46decc3eef69370c0
nltesown <nlte@magic.fr>
2015-10-05 15:43:57 +0200

    Map at bottom

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,29 @@
 $(function() {
   var ctrl = new ScrollMagic.Controller();
 
-/*
-  var elemMapContainer = document.querySelector(".mapContainer");
-  var elemMap = elemMapContainer.querySelector(".map");
+
+  // var elemMap = $("<div class='map'></div>").appendTo(document.querySelector(".mapContainer"))[0];
 
   var gm = google.maps;
 
-  var map = new gm.Map(elemMap, {
+  var mapOptions = {
     center: new gm.LatLng(40.766300, -73.977734),
     disableDefaultUI: true,
     scrollwheel: false,
     styles: [{"featureType":"administrative","elementType":"all","stylers":[{"visibility":"on"},{"lightness":33}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2e5d4"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#c5dac6"}]},{"featureType":"poi.park","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":20}]},{"featureType":"road","elementType":"all","stylers":[{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#c5c6c6"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#e4d7c6"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#fbfaf7"}]},{"featureType":"water","elementType":"all","stylers":[{"visibility":"on"},{"color":"#acbcc9"}]}], // https://snazzymaps.com/style/1/pale-dawn
     zoom: 13,
     zoomControl: true
-  });
-*/
+  };
+
+  var pin = {
+    path: "m0.05195,-0.07428c-0.63931,-3.138 -1.76633,-5.74954 -3.13148,-8.16974c-1.01259,-1.79526 -2.18562,-3.4523 -3.271,-5.19333c-0.36232,-0.58109 -0.675,-1.19516 -1.02315,-1.79822c-0.69614,-1.20605 -1.26054,-2.60439 -1.22469,-4.41824c0.03505,-1.77219 0.54759,-3.19382 1.28671,-4.35614c1.21562,-1.91174 3.25182,-3.47919 5.9839,-3.89108c2.23387,-0.33679 4.32825,0.23218 5.81332,1.10065c1.21365,0.70972 2.15358,1.65768 2.86792,2.7749c0.74567,1.16614 1.25917,2.54376 1.3022,4.34067c0.02211,0.92065 -0.12862,1.77319 -0.341,2.48038c-0.21486,0.71582 -0.5605,1.31423 -0.86803,1.95333c-0.6004,1.24765 -1.353,2.39072 -2.1084,3.53445c-2.24988,3.40698 -4.36157,6.88141 -5.28631,11.64237z",
+    fillColor: "#fff",
+    fillOpacity: 1,
+    scale: 1,
+    strokeOpacity: 1,
+    strokeColor: "#000",
+    strokeWeight: 1.5
+  };
 
 
   var items = _($(".film")).map(function (item, i) {
@@ -31,8 +39,8 @@ $(function() {
     };
   }).value();
 
-
   _.forEach(items, function (item, i) {
+
     var tween = new TimelineMax().add([
       TweenMax.to(item.elemSplash, 1, { backgroundPosition: "0% 100%", ease: Linear.easeNone }),
       TweenMax.to(item.elemOverlay, 1, { opacity: 0, ease: Power1.easeIn }),
@@ -59,36 +67,19 @@ $(function() {
       .addTo(ctrl);      
     }
 
-
     if (item.elemMapContainer) {
+      item.elemMap = $("<div class='map'></div>").appendTo(item.elemMapContainer)[0];
+      item.map = new gm.Map(item.elemMap, mapOptions); // Instantiate map
       new ScrollMagic.Scene({
         triggerElement: item.elemText,
+        // triggerHook: 0,
         triggerHook: 0.5,
         duration: 0
       })
-      .setPin(item.elemMapContainer, { pushFollowers: false })
-      .addIndicators()
+      .setPin(item.elemMapContainer, { pushFollowers: true })
+      // .addIndicators()
       .addTo(ctrl);
     }
-
-
-
-    // SlideUp map
-    /*
-    $(item.elemText).waypoint({
-      offset: "25%",
-      handler: function (dir) {
-        if (dir === "down") { // Show map
-          new TweenMax(elemMapContainer, 0.35, { top: "50%", ease: Power2.easeOut });
-        } else { // Hide map
-          new TweenMax(elemMapContainer, 0.35, { top: "100%", ease: Power2.easeOut });
-        }
-      }
-    });
-    */
-
-
-
 
 
 
